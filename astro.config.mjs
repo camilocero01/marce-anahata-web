97add5681657e7efb5284f333c3be4ec416ce15b
camilocero01 <camilocero@gmail.com>
2025-12-23 16:02:39 -0500

    optimización seo en configuración

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,16 +7,19 @@ import sitemap from '@astrojs/sitemap';
 
 import partytown from '@astrojs/partytown';
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [sitemap(), partytown()],
+  integrations: [sitemap(), partytown(), compress()],
   site: 'https://marce-anahata-web.vercel.app', // Tu URL de producción
   // 'always' fuerza la barra al final (ej: /contacto/)
   // 'never' la quita (ej: /contacto) -> Recomendado para Vercel
   trailingSlash: 'never', 
   prefetch: true, // Habilita la estrategia por defecto (hover)
+  compressHTML: true,
 });
