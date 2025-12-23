cfb593c2e31a4f094181b275d5477f650ae3d6c8
camilocero01 <camilocero@gmail.com>
2025-12-23 14:47:27 -0500

    ajustes seo + google analityc

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [sitemap()],
+  integrations: [sitemap(), partytown()],
   site: 'https://marce-anahata-web.vercel.app', // Tu URL de producción
+  // 'always' fuerza la barra al final (ej: /contacto/)
+  // 'never' la quita (ej: /contacto) -> Recomendado para Vercel
+  trailingSlash: 'never', 
+  prefetch: true, // Habilita la estrategia por defecto (hover)
 });
