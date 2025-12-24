a9edce77a48a692744bc4d3f55b347c97fa04d5e
camilocero01 <camilocero@gmail.com>
2025-12-24 08:07:07 -0500

    deshabilitar la inyección de estilos en el HTML en la configuración de build

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,6 @@ export default defineConfig({
   build: {
     // 'auto' inyecta estilos pequeños en el HTML y deja los grandes en archivos aparte.
     // 'always' inyecta TODO el CSS en el HTML (bueno para sitios pequeños).
-    inlineStylesheets: 'always', 
+    //inlineStylesheets: 'always', 
   }
 });
