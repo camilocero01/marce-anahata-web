75ca8d623a2a902f0a6cc63637bafb7a5f01ae0a
camilocero01 <camilocero@gmail.com>
2026-01-05 14:21:21 -0500

    feat: add type assertions for form elements in Newsletter component

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
   schema: z.object({
     title: z.string(),
+    seoTitle: z.string().optional(),
     description: z.string(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
